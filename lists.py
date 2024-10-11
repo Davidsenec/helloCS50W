60d600270538fcfa79d1ecacf3d71906c6710e77
Davidsenec <sgd88.thomponsenec@yahoo.com>
2024-10-11 18:34:33 +0700

    added new files

---
+++ lists.py
@@ -0,0 +1,9 @@
+# Define a list of names
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+coordinate = (10.0, 20,0)
+names.append("Draco")
+
+names.sort()
+print(names)
+print(names[0], names[2])
+print(coordinate)
